56c72a8a5fae0176c7753f5155609a13802a77bd
DavidOBr <david.obrien.nord@gmail.com>
2023-09-12 00:00:13 +0100

    implemented the api. TODO unit tests

--- tests/tests.py
+++ tests/tests.py
@@ -6,3 +6,9 @@ class ViewsCanRenderTest(TestCase):
     def test_can_render_homepage(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+
+
+class WeatherApiTest(TestCase):
+    def test_correct_api_response(self):
+        response = self.client.get('https://api.openweathermap.org/data/2.5/weather?q=Lima&appid=cc9992194592a47721720513309df459&units=metric')
+        self.assertEqual(response.status_code, 200)
--- website/views.py
+++ website/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+import requests
+import api_key as key
+
+api_key = key.API
 
 
 def homepage(request):
-    return render(request, 'website/homepage.html')
+    if request.method == "POST":
+        city = request.POST.get('city')  # Replace 'city' with the name of your form input field
+        url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
+        response = requests.get(url)
+        weather_data = response.json()
+        return render(request, 'website/homepage.html', {'weather_data': weather_data})
+    else:
+        return render(request, 'website/homepage.html')
